b9208688282685099a6e61825343c5b455e6fea7
ReeceHoffmann <reecehoffmann@outlook.com>
2021-05-06 20:05:56 -0700

    5 files modified, 10 added

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+  addToCart() {
+    this.cartService.addToCart(this.product);
+    window.alert('Your ' + this.product.name + ' Has been added to your cart');
+  }
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+}
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent {
-
-}
-
+export class TopBarComponent {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
