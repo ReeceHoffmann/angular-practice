5ff3143819d2b531aceef98878974514c9cc438a
ReeceHoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2021-05-17 15:43:28 -0700

    implemented messages

--- angular-tour-of-heros/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
 import {HeroService} from '../hero.service'
+import {MessageService} from "../message.service"
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,17 +9,21 @@ import {HeroService} from '../hero.service'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+              private messageService: MessageService
+              ) { }
   heroes: Hero[] = [];
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes= heroes);
   }
   curHero?: Hero;
+ 
   ngOnInit(): void {
     this.getHeroes();
   }
   
   onSelect(hero:Hero){
+    this.messageService.add(`HeroesComponent: Selected Hero id= ${hero.id}. name = ${hero.name}`)
     this.curHero = hero;
   }
 
